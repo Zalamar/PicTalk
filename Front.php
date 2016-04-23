40ca8e336f3bb86dde82645daf7ff40bb1e64a0d
skole <skole@localhost>
2016-04-23 20:20:15 +0200

    ¨ ¨

--- Front.php
+++ Front.php
@@ -1,8 +1,9 @@
+<!DOCTYPE html>
 <html>
     <head>
         <title>Homepage</title>
     <div id="header">
-    <img src="Pictalklogo.png" style="width:200px;height:150px;"> 
+        <img src="Pictalklogo.png" style="width:200px;height:150px;"> 
     </div>
 </head>
 <?php
@@ -23,21 +24,104 @@ echo "Today is " . date("l-d-m-y") . "<br>";
             <table width=100%>  
                 <DIV>
                     <TD>
-                    <td><CENTER><button><a href="About.php.html">ABOUT</a></button></CENTER></td>
+                    <td><CENTER><button><a href="About.php">ABOUT</a></button></CENTER></td>
                     <td><CENTER><button><a>FOLLOWING</a></button></button></CENTER></td>
-                    <td><CENTER><button><a>FOLLOWERS</a></button></button></CENTER></td>
+                    <td><CENTER><button><a href="Profil.php">Profile</a></button></button></CENTER></td>
                     <td><CENTER><button><a>SETTINGS</a></button></button></CENTER></td>
                     </TD>
                 </DIV>
             </table>
         </CENTER></div>
     <br>
-    <div>
-        <table align="right">
-            <fieldset style='width:35%'>
-                </div>
-                i am lord. lord, lord, lord.
+        <table>
+            <style>
+                div.scroll {
+                    background-color: #white;
+                    width: 1000px;
+                    height: 1000px;
+                    overflow: scroll;
+                }
+            </style>
+            <div class="scroll">
+                <p>Oh, misty eye of the mountain below
+                    <br>Keep careful watch of my brothers' souls
+                    <br>And should the sky be filled with fire and smoke
+                    <br>Keep watching over Durin's sons
+                    <br>
+                    <br>If this is to end in fire
+                    <br>Then we should all burn together
+                    <br>Watch the flames climb high into the night
+                    <br>
+                    <br>Calling out father oh
+                    <br>Stand by and we will
+                    <br>Watch the flames burn auburn on
+                    <br>The mountain side
+                    <br>
+                    <br>And if we should die tonight
+                    <br>Then we should all die together
+                    <br>Raise a glass of wine for the last time
+                    <br>
+                    <br>Calling out father oh
+                    <br>Prepare as we will
+                    <br>Watch the flames burn auburn on
+                    <br>The mountain side
+                    <br>Desolation comes upon the sky
+                    <br>
+                    <br>Now I see fire
+                    <br>Inside the mountain
+                    <br>I see fire
+                    <br>Burning the trees
+                    <br>And I see fire
+                    <br>Hollowing souls
+                    <br>I see fire
+                    <br>Blood in the breeze
+                    <br>And I hope that you remember me
+                    <br>
+                    <br>Oh, should my people fall
+                    <br>Then surely I'll do the same
+                    <br>Confined in mountain halls
+                    <br>We got too close to the flame
+                    <br>
+                    <br>Calling out father oh
+                    <br>Hold fast and we will
+                    <br>Watch the flames burn auburn on
+                    <br>The mountain side
+                    <br>Desolation comes upon the sky
+                    <br>
+                    <br>Now I see fire
+                    <br>Inside the mountain
+                    <br>I see fire
+                    <br>Burning the trees
+                    <br>I see fire
+                    <br>Hollowing souls
+                    <br>I see fire
+                    <br>Blood in the breeze
+                    <br>And I hope that you remember me
+                    <br>
+                    <br>And if the night is burning
+                    <br>I will cover my eyes
+                    <br>For if the dark returns
+                    <br>Then my brothers will die
+                    <br>And as the sky is falling down
+                    <br>It crashed into this lonely town
+                    <br>And with that shadow upon the ground
+                    <br>I hear my people screaming out
+                    <br>
+                    <br>Now I see fire
+                    <br>Inside the mountains
+                    <br>I see fire
+                    <br>Burning the trees
+                    <br>I see fire
+                    <br>Hollowing souls
+                    <br>I see fire
+                    <br>Blood in the breeze
+                    <br>
+                    <br>I see fire (oh you know I saw a city burning out) (fire)
+                    <br>And I see fire (feel the heat upon my skin, yeah) (fire)
+                    <br>And I see fire (uh-uh-uh-uh) (fire)
+                    <br>and I see fire burn auburn on the mountain side</p>              
         </table>
+
         <table>
             <div><fieldset style='width:35%'>
                     <p>herp de derp</p>
@@ -45,25 +129,8 @@ echo "Today is " . date("l-d-m-y") . "<br>";
                     <p>herp de derp</p>
                     <p>herp de derp</p>
                     <p>herp de derp</p>
-                    <p>herp de derp</p>
-                    <p>herp de derp</p>
-                    <p>herp de derp</p>
-                    <p>herp de derp</p>
-                    <p>herp de derp</p>
-                    <p>herp de derp</p>
-                    <p>herp de derp</p>
-                    <p>herp de derp</p>
                     </table>
-
-                    <table>
-                        <div><fieldset style='width:35%'>
-                                <p>herp de derp</p>
-                                <p>herp de derp</p>
-                                <p>herp de derp</p>
-                                <p>herp de derp</p>
-                                <p>herp de derp</p>
-                                </table>
-                                </body>
-                                </html>
+                    </body>
+                    </html>
 
 
