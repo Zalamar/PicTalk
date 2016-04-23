a01f208c44fda9a794655bce92e8f5bceb527e4d
skole <skole@localhost>
2016-04-23 20:30:10 +0200

    ¨ ¨

---
+++ Settings.php
@@ -0,0 +1,37 @@
+<html>
+    <head>
+        <title>Homepage</title>
+    <div id="header">
+        <a href="Front.php">
+            <img src="Pictalklogo.png" style="width:200px;height:150px;"> 
+        </a>   
+    </div>
+    <?php
+    echo "Today is " . date("l-d-m-y") . "<br>";
+    ?>
+</head>
+<body>  
+    <style>
+        table {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+        th, td {
+            padding: 20px;
+        }
+    </style>
+
+<CENTER><div id='Sign-In'><fieldset style='width:99,5%'> 
+            <table width=100%>  
+                <DIV>
+                    <TD>
+                    <td><CENTER><button><a href="About.php">ABOUT</a></button></CENTER></td>
+                    <td><CENTER><button><a>FOLLOWING</a></button></button></CENTER></td>
+                    <td><CENTER><button><a href="Profil.php">Profile</a></button></button></CENTER></td>
+                <td><CENTER><button><a href="Settings.php">SETTINGS</a></button></button></CENTER></td>
+                    </TD>
+                </DIV>
+            </table>
+        </CENTER></div>
+</body>
+</html>
