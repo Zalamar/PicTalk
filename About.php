fe990b1a95fcdea6fd0bf83e42869edf2cb2d2b1
skole <skole@localhost>
2016-04-23 20:30:30 +0200

    ¨ ¨

--- About.php
+++ About.php
@@ -2,13 +2,15 @@
     <head>
         <title>Homepage</title>
     <div id="header">
-        <img href="Front.php" src="Pictalklogo.png" style="width:200px;height:150px;"> 
+        <a href="Front.php">
+            <img src="Pictalklogo.png" style="width:200px;height:150px;"> 
+        </a>   
     </div>
     <?php
-echo "Today is " . date("l-d-m-y") . "<br>";
-?>
+    echo "Today is " . date("l-d-m-y") . "<br>";
+    ?>
 </head>
-<body>      
+<body>  
     <style>
         table {
             border: 1px solid black;
@@ -26,99 +28,99 @@ echo "Today is " . date("l-d-m-y") . "<br>";
                     <td><CENTER><button><a href="About.php">ABOUT</a></button></CENTER></td>
                     <td><CENTER><button><a>FOLLOWING</a></button></button></CENTER></td>
                     <td><CENTER><button><a href="Profil.php">Profile</a></button></button></CENTER></td>
-                    <td><CENTER><button><a>SETTINGS</a></button></button></CENTER></td>
+                    <td><CENTER><button><a href="Settings.php">SETTINGS</a></button></button></CENTER></td>
                     </TD>
                 </DIV>
             </table>
         </CENTER></div>
-     <style>
-                div.scroll {
-                    background-color: #white;
-                    width: 1000px;
-                    height: 1000px;
-                    alignment-adjust: "center";
-                    overflow: scroll;
-                }
-            </style>
-            <div class="scroll">
-                <p>The webpage PicTalk is a school project
-                    <br>Made by 3 high school students
-                    <br>Named - Thor Bjorn gabe, Tobias Rydberg and Victor Mtsimbe Norrild.
-                    <br>
-                    <br>It was made for our finals in the a class
-                    <br>called "Information Technologi".
-                    <br>It was required that it held 
-                    <br>a image file, a login system, a database, tables, list, logos/self made vidoes og other things.
-                    <br>
-                    <br>The project is to delivered on the 24th of April.
-                    <br>On our schools platform. 
-                    <br>
-                    <br>Further information awaits, in the meanwhile, heres some ed sheeran.
-                    <br>
-                    <br>
-                    <br>And if we should die tonight
-                    <br>Then we should all die together
-                    <br>Raise a glass of wine for the last time
-                    <br>
-                    <br>Calling out father oh
-                    <br>Prepare as we will
-                    <br>Watch the flames burn auburn on
-                    <br>The mountain side
-                    <br>Desolation comes upon the sky
-                    <br>
-                    <br>Now I see fire
-                    <br>Inside the mountain
-                    <br>I see fire
-                    <br>Burning the trees
-                    <br>And I see fire
-                    <br>Hollowing souls
-                    <br>I see fire
-                    <br>Blood in the breeze
-                    <br>And I hope that you remember me
-                    <br>
-                    <br>Oh, should my people fall
-                    <br>Then surely I'll do the same
-                    <br>Confined in mountain halls
-                    <br>We got too close to the flame
-                    <br>
-                    <br>Calling out father oh
-                    <br>Hold fast and we will
-                    <br>Watch the flames burn auburn on
-                    <br>The mountain side
-                    <br>Desolation comes upon the sky
-                    <br>
-                    <br>Now I see fire
-                    <br>Inside the mountain
-                    <br>I see fire
-                    <br>Burning the trees
-                    <br>I see fire
-                    <br>Hollowing souls
-                    <br>I see fire
-                    <br>Blood in the breeze
-                    <br>And I hope that you remember me
-                    <br>
-                    <br>And if the night is burning
-                    <br>I will cover my eyes
-                    <br>For if the dark returns
-                    <br>Then my brothers will die
-                    <br>And as the sky is falling down
-                    <br>It crashed into this lonely town
-                    <br>And with that shadow upon the ground
-                    <br>I hear my people screaming out
-                    <br>
-                    <br>Now I see fire
-                    <br>Inside the mountains
-                    <br>I see fire
-                    <br>Burning the trees
-                    <br>I see fire
-                    <br>Hollowing souls
-                    <br>I see fire
-                    <br>Blood in the breeze
-                    <br>
-                    <br>I see fire (oh you know I saw a city burning out) (fire)
-                    <br>And I see fire (feel the heat upon my skin, yeah) (fire)
-                    <br>And I see fire (uh-uh-uh-uh) (fire)
-                    <br>and I see fire burn auburn on the mountain side</p>              
+    <style>
+        div.scroll {
+            background-color: #white;
+            width: 1000px;
+            height: 1000px;
+            alignment-adjust: "center";
+            overflow: scroll;
+        }
+    </style>
+    <div class="scroll">
+        <p>The webpage PicTalk is a school project
+            <br>Made by 3 high school students
+            <br>Named - Thor Bjorn gabe, Tobias Rydberg and Victor Mtsimbe Norrild.
+            <br>
+            <br>It was made for our finals in the a class
+            <br>called "Information Technologi".
+            <br>It was required that it held 
+            <br>a image file, a login system, a database, tables, list, logos/self made vidoes og other things.
+            <br>
+            <br>The project is to delivered on the 24th of April.
+            <br>On our schools platform. 
+            <br>
+            <br>Further information awaits, in the meanwhile, heres some ed sheeran.
+            <br>
+            <br>
+            <br>And if we should die tonight
+            <br>Then we should all die together
+            <br>Raise a glass of wine for the last time
+            <br>
+            <br>Calling out father oh
+            <br>Prepare as we will
+            <br>Watch the flames burn auburn on
+            <br>The mountain side
+            <br>Desolation comes upon the sky
+            <br>
+            <br>Now I see fire
+            <br>Inside the mountain
+            <br>I see fire
+            <br>Burning the trees
+            <br>And I see fire
+            <br>Hollowing souls
+            <br>I see fire
+            <br>Blood in the breeze
+            <br>And I hope that you remember me
+            <br>
+            <br>Oh, should my people fall
+            <br>Then surely I'll do the same
+            <br>Confined in mountain halls
+            <br>We got too close to the flame
+            <br>
+            <br>Calling out father oh
+            <br>Hold fast and we will
+            <br>Watch the flames burn auburn on
+            <br>The mountain side
+            <br>Desolation comes upon the sky
+            <br>
+            <br>Now I see fire
+            <br>Inside the mountain
+            <br>I see fire
+            <br>Burning the trees
+            <br>I see fire
+            <br>Hollowing souls
+            <br>I see fire
+            <br>Blood in the breeze
+            <br>And I hope that you remember me
+            <br>
+            <br>And if the night is burning
+            <br>I will cover my eyes
+            <br>For if the dark returns
+            <br>Then my brothers will die
+            <br>And as the sky is falling down
+            <br>It crashed into this lonely town
+            <br>And with that shadow upon the ground
+            <br>I hear my people screaming out
+            <br>
+            <br>Now I see fire
+            <br>Inside the mountains
+            <br>I see fire
+            <br>Burning the trees
+            <br>I see fire
+            <br>Hollowing souls
+            <br>I see fire
+            <br>Blood in the breeze
+            <br>
+            <br>I see fire (oh you know I saw a city burning out) (fire)
+            <br>And I see fire (feel the heat upon my skin, yeah) (fire)
+            <br>And I see fire (uh-uh-uh-uh) (fire)
+            <br>and I see fire burn auburn on the mountain side</p>              
         </table>
-</body>
-</html>
+        </body>
+        </html>
