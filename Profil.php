059098790322fb622c2fec546b3636e28c0c2c03
skole <skole@localhost>
2016-04-23 20:19:56 +0200

    ¨ ¨

--- Profil.php
+++ Profil.php
@@ -2,20 +2,20 @@
     <head>
         <title>Profile</title>
     <div id="header">
-    <img src="Pictalklogo.png" style="width:200px;height:150px;"> 
+        <img src="Pictalklogo.png" style="width:200px;height:150px;"> 
     </div>
     <?php
-echo "Today is " . date("l-d-m-y") . "<br>";
-?>
+    echo "Today is " . date("l-d-m-y") . "<br>";
+    ?>
 </head>
 <body
 <Center><div id='Sign-In'> <fieldset style='width:99,5%'> 
             <table width=100%>  
                 <DIV>
                     <TD>
-                    <td><CENTER><button><a>ABOUT</a></button></CENTER></td>
+                    <td><CENTER><button><a href="About.php">ABOUT</a></button></CENTER></td>
                     <td><CENTER><button><a>FOLLOWING</a></button></button></CENTER></td>
-                    <td><CENTER><button><a>FOLLOWERS</a></button></button></CENTER></td>
+                    <td><CENTER><button><a href="Profil.php">Profile</a></button></button></CENTER></td>
                     <td><CENTER><button><a>SETTINGS</a></button></button></CENTER></td>
                     </TD>
                 </DIV>
@@ -36,44 +36,48 @@ echo "Today is " . date("l-d-m-y") . "<br>";
 
 <table style="width:33%" border='1'>
     <tr>
-            <style>
-    table {
-        border: 1px solid black;
-        border-collapse: collapse;
-    }
-    th, td {
-        padding: 20px;
-    }
-</style>
-<td>
-            <?php
-            $age = array("Peter" => "35");
-            echo "Age - " . $age['Peter'];
-            ?>
-        </td>
-        <td>
-            <?php
-            $follower = array ("Peter"=> "10");
-            echo "Follower count:" . $follwer["Peter"];
-                    ?>
-    </tr>
-    <tr>
-        <td>
-            <?php
-            $nummer = array("Peter" => "12 34 56 78");
-            echo "Phone number - " . $nummer['Peter'];
-            ?>
-        </td> 
-    </tr>
-    <tr>
-        <td>
-            <?php
-            $gender = array("Peter" => "Male", "Jannis" => "Female");
-            echo "Gender - " . $gender['Peter'];
-            ?>
-        </td>
-    <br>
-    <br>
+    <style>
+        table {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+        th, td {
+            padding: 20px;
+        }
+    </style>
+    <td>
+        <?php
+        $age = array("Peter" => "35");
+        echo "Age - " . $age['Peter'];
+        ?>
+    </td>
+</tr>
+<tr>
+    <td>
+        <?php
+        $nummer = array("Peter" => "12 34 56 78");
+        echo "Phone number - " . $nummer['Peter'];
+        ?>
+    </td> 
+</tr>
+<tr>
+    <td>
+        <?php
+        $gender = array("Peter" => "Male", "Jannis" => "Female");
+        echo "Gender - " . $gender['Peter'];
+        ?>
+    </td>
+<tr>
+    <td>
 
-    </body>
-    </html>
+        <?php
+        $nummer = array("Peter" => "10");
+        echo "Follower count:" . $nummer["Peter"];
+        ?>
+    </td>
+</tr>
+<br>
+<br>
+
+</body>
+</html>
