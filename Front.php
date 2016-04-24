74a78cad1e2014c5ef2b8a8887dbb74eeb352256
skole <skole@localhost>
2016-04-24 15:48:52 +0200

    ¨ ¨

--- Front.php
+++ Front.php
@@ -4,7 +4,7 @@
         <title>Homepage</title>
     <div id="header">
         <a href="Front.php">
-        <img src="Pictalklogo.png" style="width:200px;height:150px;"> 
+            <img src="Pictalklogo.png" style="width:200px;height:150px;"> 
         </a>
     </div>
 </head>
@@ -34,105 +34,109 @@ echo "Today is " . date("l-d-m-y") . "<br>";
             </table>
         </CENTER></div>
     <br>
-     <h1 style="text-align:center;">WELCOME</h1>
-        <table>
+    <h1 style="text-align:center;">WELCOME</h1>
+    <table>
+        <div class="scroll">
             <style>
                 div.scroll {
-                    background-color: #white;
-                    width: 1000px;
-                    height: 1000px;
+                    border: solid 1px black;
+                    position: fixed;
+                    left: 50%;
+                    top: 70%;
+                    background-color: white;
+                    z-index: 100;
                     overflow: scroll;
+                    height: 500px;
+                    margin-top: -200px;
+
+                    width: 700px;
+                    margin-left: -300px;
                 }
             </style>
-            <div class="scroll">
-                <p>Oh, misty eye of the mountain below
-                    <br>Keep careful watch of my brothers' souls
-                    <br>And should the sky be filled with fire and smoke
-                    <br>Keep watching over Durin's sons
-                    <br>
-                    <br>If this is to end in fire
-                    <br>Then we should all burn together
-                    <br>Watch the flames climb high into the night
-                    <br>
-                    <br>Calling out father oh
-                    <br>Stand by and we will
-                    <br>Watch the flames burn auburn on
-                    <br>The mountain side
-                    <br>
-                    <br>And if we should die tonight
-                    <br>Then we should all die together
-                    <br>Raise a glass of wine for the last time
-                    <br>
-                    <br>Calling out father oh
-                    <br>Prepare as we will
-                    <br>Watch the flames burn auburn on
-                    <br>The mountain side
-                    <br>Desolation comes upon the sky
-                    <br>
-                    <br>Now I see fire
-                    <br>Inside the mountain
-                    <br>I see fire
-                    <br>Burning the trees
-                    <br>And I see fire
-                    <br>Hollowing souls
-                    <br>I see fire
-                    <br>Blood in the breeze
-                    <br>And I hope that you remember me
-                    <br>
-                    <br>Oh, should my people fall
-                    <br>Then surely I'll do the same
-                    <br>Confined in mountain halls
-                    <br>We got too close to the flame
-                    <br>
-                    <br>Calling out father oh
-                    <br>Hold fast and we will
-                    <br>Watch the flames burn auburn on
-                    <br>The mountain side
-                    <br>Desolation comes upon the sky
-                    <br>
-                    <br>Now I see fire
-                    <br>Inside the mountain
-                    <br>I see fire
-                    <br>Burning the trees
-                    <br>I see fire
-                    <br>Hollowing souls
-                    <br>I see fire
-                    <br>Blood in the breeze
-                    <br>And I hope that you remember me
-                    <br>
-                    <br>And if the night is burning
-                    <br>I will cover my eyes
-                    <br>For if the dark returns
-                    <br>Then my brothers will die
-                    <br>And as the sky is falling down
-                    <br>It crashed into this lonely town
-                    <br>And with that shadow upon the ground
-                    <br>I hear my people screaming out
-                    <br>
-                    <br>Now I see fire
-                    <br>Inside the mountains
-                    <br>I see fire
-                    <br>Burning the trees
-                    <br>I see fire
-                    <br>Hollowing souls
-                    <br>I see fire
-                    <br>Blood in the breeze
-                    <br>
-                    <br>I see fire (oh you know I saw a city burning out) (fire)
-                    <br>And I see fire (feel the heat upon my skin, yeah) (fire)
-                    <br>And I see fire (uh-uh-uh-uh) (fire)
-                    <br>and I see fire burn auburn on the mountain side</p>              
-        </table>
+            <p allign="center">
+                    Oh, misty eye of the mountain below
+                <br>Keep careful watch of my brothers' souls
+                <br>And should the sky be filled with fire and smoke
+                <br>Keep watching over Durin's sons
+                <br>
+                <br>If this is to end in fire
+                <br>Then we should all burn together
+                <br>Watch the flames climb high into the night
+                <br>
+                <br>Calling out father oh
+                <br>Stand by and we will
+                <br>Watch the flames burn auburn on
+                <br>The mountain side
+                <br>
+                <br>And if we should die tonight
+                <br>Then we should all die together
+                <br>Raise a glass of wine for the last time
+                <br>
+                <br>Calling out father oh
+                <br>Prepare as we will
+                <br>Watch the flames burn auburn on
+                <br>The mountain side
+                <br>Desolation comes upon the sky
+                <br>
+                <br>Now I see fire
+                <br>Inside the mountain
+                <br>I see fire
+                <br>Burning the trees
+                <br>And I see fire
+                <br>Hollowing souls
+                <br>I see fire
+                <br>Blood in the breeze
+                <br>And I hope that you remember me
+                <br>
+                <br>Oh, should my people fall
+                <br>Then surely I'll do the same
+                <br>Confined in mountain halls
+                <br>We got too close to the flame
+                <br>
+                <br>Calling out father oh
+                <br>Hold fast and we will
+                <br>Watch the flames burn auburn on
+                <br>The mountain side
+                <br>Desolation comes upon the sky
+                <br>
+                <br>Now I see fire
+                <br>Inside the mountain
+                <br>I see fire
+                <br>Burning the trees
+                <br>I see fire
+                <br>Hollowing souls
+                <br>I see fire
+                <br>Blood in the breeze
+                <br>And I hope that you remember me
+                <br>
+                <br>And if the night is burning
+                <br>I will cover my eyes
+                <br>For if the dark returns
+                <br>Then my brothers will die
+                <br>And as the sky is falling down
+                <br>It crashed into this lonely town
+                <br>And with that shadow upon the ground
+                <br>I hear my people screaming out
+                <br>
+                <br>Now I see fire
+                <br>Inside the mountains
+                <br>I see fire
+                <br>Burning the trees
+                <br>I see fire
+                <br>Hollowing souls
+                <br>I see fire
+                <br>Blood in the breeze
+                <br>
+                <br>I see fire (oh you know I saw a city burning out) (fire)
+                <br>And I see fire (feel the heat upon my skin, yeah) (fire)
+                <br>And I see fire (uh-uh-uh-uh) (fire)
+                <br>and I see fire burn auburn on the mountain side</p>              
+    </table>
+    <br>
+    <br>
+    </body>
+    </html>
 
-        <table>
-            <div><fieldset style='width:35%'>
-                    <p>herp de derp</p>
-                    <p>herp de derp</p>
-                    <p>herp de derp</p>
-                    <p>herp de derp</p>
-                    <p>herp de derp</p>
-                    </table>
-                    </body>
-                    </html>
+  
 
 
